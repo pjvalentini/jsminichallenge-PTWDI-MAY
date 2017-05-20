f7bedb0c79643c6a194c991bcf34f4767227133f
pjvalentini <pj.valentini@gmail.com>
2017-05-20 18:25:19 -0400

    Updates to JS File

--- JavaScript/minichall.js
+++ JavaScript/minichall.js
@@ -42,24 +42,28 @@ if (number < 100) {
 /* 4 Declare a function that takes a name as an argument and tells
 the user what name they've entered. Try running it after it has been declared.*/
 
-// Created var with name "Philip"
+// Created var with name "Philip", this will be the name that has been entered
 var userName = "Philip";
-function enteredName(name) {
+function printName(name) {
 	console.log(name + " is the name that has been entered!");
 }
+// Created var names with an Array of names assigned to it.
+var names = ["Bill", "PJ", "Jen"];
 // Run the function enteredName() with a string to confirm that the name entered is "Philip".
-enteredName(userName);
+printName(userName);
+// Passing in the indexes of the array in to the function.
+printName(names[0]); // prints "Bill is the name that as been entered"
+printName(names[1]); // prints "PJ is the name that as been entered"
+printName(names[2]); // prints "Jen is the name that as been entered"
 
 
 // Or this is another way to do this exercise.....
-
 function person(name) {
   return "You have entered the name" + name;
 }
-
-console.log(person(" PJ "));
-console.log(person(" Bill "));
-console.log(person(" Joe "));
+console.log(person(" PJ."));
+console.log(person(" Bill."));
+console.log(person(" Joe."));
 
 
 
